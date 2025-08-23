a71b19e35fb8a3b278fd17cad0d1cdee7aa027ec
Dodo-tech001 <ajiboladavid07@gmail.com>
2025-08-23 20:42:56 +0100

    Refactor the nav bar

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "referrer-policy", value: "no-referrer" }],
+      },
+    ];
+  },
+};
 require("@tailwindcss/typography");
 
 module.exports = nextConfig;
